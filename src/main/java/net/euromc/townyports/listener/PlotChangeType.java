1bacc275dc1e0d6a11635611eeb8ae315ed57708
Bustun01 <159726565+Bustun01@users.noreply.github.com>
2025-04-19 08:05:04 -0400

    PlotChangeType.class - enable multiple types of port plots
    - Multiple types of biomes can be defined in eligibleBiomes
    - If the biome's name contains a certain keyword (as defined in eligibleBiomes), it will be considered eligible.

--- src/main/java/net/euromc/townyports/listener/PlotChangeType.java
+++ src/main/java/net/euromc/townyports/listener/PlotChangeType.java
@@ -8,6 +8,8 @@ import org.bukkit.block.Biome;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.util.Set;
+
 public class PlotChangeType implements Listener {
 
     private static final int halfPlotSize = TownySettings.getTownBlockSize() / 2;
@@ -16,19 +18,40 @@ public class PlotChangeType implements Listener {
     public void onPlotChangeType(PlotPreChangeTypeEvent event) {
         if (!event.getNewType().getName().equals("port"))
             return;
-        if (isInOceanBiome(event.getTownBlock().getWorldCoord()))
+        if (isEligibleBiome(event.getTownBlock().getWorldCoord()))
             return;
         event.setCancelled(true);
         event.setCancelMessage("§cYou cannot set plots to port type outside of ocean biomes.");
     }
 
-	private boolean isInOceanBiome(WorldCoord wc) {
-		return getBiome(wc).name().contains("OCEAN");
+	// Determines if a biome is eligible to have a port plot in it.
+    private boolean isEligibleBiome(WorldCoord wc) {
+
+        // Oceans, rivers, and beaches are eligible to be port plots.
+        final Set<String> eligibleBiomes = Set.of(
+                "ocean",
+                "river",
+                "beach"
+        );
+
+        /* name() is deprecated as of 1.21, will be removed in 1.22.
+        * getBiome(wc) returns an OldEnum biome, which is also deprecated. This will need to be refactored
+        * in the future.
+        * */
+        @Deprecated
+        String biomeName = getBiome(wc).name().toLowerCase();
+
+        for (String biome : eligibleBiomes) {
+            if (biomeName.contains(biome))  {return true;}
+        }
+        return false;
+
 	}
-    
+
     private Biome getBiome(WorldCoord wc) {
         return wc.getBukkitWorld().getBiome(wc.getLowerMostCornerLocation().add(halfPlotSize, 61, halfPlotSize));
     }
+
 }
 
 
