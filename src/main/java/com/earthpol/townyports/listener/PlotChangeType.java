32e7b3cfbe590b05c7ac84e950624ed2680def84
Bustun01 <159726565+Bustun01@users.noreply.github.com>
2025-06-11 02:35:09 -0400

    Update biome determination logic
    Testing required

--- src/main/java/com/earthpol/townyports/listener/PlotChangeType.java
+++ src/main/java/com/earthpol/townyports/listener/PlotChangeType.java
@@ -2,8 +2,10 @@ package com.earthpol.townyports.listener;
 
 import com.palmergames.bukkit.towny.TownySettings;
 import com.palmergames.bukkit.towny.event.*;
+import com.palmergames.bukkit.towny.object.TownBlock;
 import com.palmergames.bukkit.towny.object.WorldCoord;
 
+import org.bukkit.Chunk;
 import org.bukkit.block.Biome;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,46 +14,48 @@ import java.util.Set;
 
 public class PlotChangeType implements Listener {
 
-    private static final int halfPlotSize = TownySettings.getTownBlockSize() / 2;
+    // Oceans, rivers, and beaches are eligible to be port plots.
+    private static final Set<Biome> eligibleBiomes = Set.of(
+            Biome.OCEAN,
+            Biome.WARM_OCEAN,
+            Biome.LUKEWARM_OCEAN,
+            Biome.COLD_OCEAN,
+            Biome.FROZEN_OCEAN,
+            Biome.DEEP_OCEAN,
+            Biome.DEEP_LUKEWARM_OCEAN,
+            Biome.DEEP_COLD_OCEAN,
+            Biome.DEEP_FROZEN_OCEAN,
+            Biome.RIVER,
+            Biome.FROZEN_RIVER,
+            Biome.BEACH,
+            Biome.SNOWY_BEACH
+    );
+
 
 	@EventHandler
     public void onPlotChangeType(PlotPreChangeTypeEvent event) {
-        if (!event.getNewType().getName().equals("port"))
-            return;
-        if (isEligibleBiome(event.getTownBlock().getWorldCoord()))
-            return;
-        event.setCancelled(true);
-        event.setCancelMessage("§cYou cannot set plots to port type outside of ocean biomes.");
+        //Ignore non-port plot types
+        if (!event.getNewType().getName().equals("port")) return;
+        
+        if (!isEligibleBiome(event.getTownBlock())){
+            event.setCancelled(true);
+            event.setCancelMessage("§cYou cannot set plots to port type outside of ocean biomes.");
+        }
     }
 
 	// Determines if a biome is eligible to have a port plot in it.
-    private boolean isEligibleBiome(WorldCoord wc) {
+    private boolean isEligibleBiome(TownBlock townBlock) {
+        Chunk potentialPortChunk = townBlock.getWorldCoord().getLowerMostCornerLocation().getChunk();
 
-        // Oceans, rivers, and beaches are eligible to be port plots.
-        final Set<String> eligibleBiomes = Set.of(
-                "ocean",
-                "river",
-                "beach"
-        );
-
-        /* name() is deprecated as of 1.21, will be removed in 1.22.
-        * getBiome(wc) returns an OldEnum biome, which is also deprecated. This will need to be refactored
-        * in the future.
-        * */
-        @Deprecated
-        String biomeName = getBiome(wc).name().toLowerCase();
-
-        //Compare each entry of eligibleBiomes to the provided biomeName
-        for (String biome : eligibleBiomes) {
-            if (biomeName.contains(biome))  {return true;}
+        for (Biome biome : eligibleBiomes){
+            if(potentialPortChunk.contains(biome)){
+                return true;
+            }
         }
         return false;
-
 	}
 
-    private Biome getBiome(WorldCoord wc) {
-        return wc.getBukkitWorld().getBiome(wc.getLowerMostCornerLocation().add(halfPlotSize, 61, halfPlotSize));
-    }
+    // TODO: Redo biome determination
 
 }
 
