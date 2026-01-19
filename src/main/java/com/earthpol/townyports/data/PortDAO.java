43fc5f9fa8b0a3bd59787c44055b1b62ef736767
Bustun01 <159726565+Bustun01@users.noreply.github.com>
2026-01-19 18:16:25 -0500

    Support setting port price and spawn separately

--- src/main/java/com/earthpol/townyports/data/PortDAO.java
+++ src/main/java/com/earthpol/townyports/data/PortDAO.java
@@ -1,6 +1,9 @@
 package com.earthpol.townyports.data;
 
 import com.earthpol.earthPolLib.location.LocationUtil;
+import com.earthpol.townyports.PortsMain;
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.object.Resident;
 import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.metadata.DecimalDataField;
 import com.palmergames.bukkit.towny.object.metadata.LocationDataField;
@@ -18,61 +21,34 @@ public final class PortDAO {
 
     // Return a Port object built from the corresponding Towny metadata fields.
     public static @Nullable Port getPort(Town town){
-        // If either of the metadata fields are missing, the town is treated as not having a port. Null port will be returned.
+
+        // If the port location metadata is missing, then return null port.
         if (!MetaDataUtil.hasMeta(town,portLocationField)) return null;
-        if (MetaDataUtil.hasMeta(town,portPriceField)) return null;
+
+        // If there is no price metadata, the price will just be whatever the default price is.
+        double portPrice;
+        if (!MetaDataUtil.hasMeta(town,portPriceField)){
+            portPrice = PortsMain.getCustomConfig().getDouble("default-port-fee");
+        }
+        else portPrice = MetaDataUtil.getDouble(town,portPriceField);
 
         return new Port(
                 MetaDataUtil.getLocation(town,portLocationField),
                 town,
-                MetaDataUtil.getDouble(town,portPriceField)
+                portPrice
         );
     }
 
-    public static boolean hasPort(Town town){return getPort(town) != null;}
-
-    // Sets the port location for a town.
-    public static void setPort(Town town,
-                               Location location,
-                               double price,
-                               @Nullable Player p){
-
-        // Extra checks and player-facing error messaging are done if this is called by a player.
-        boolean calledByPlayer = (p != null);
-
-        // Permissions check
-        if(calledByPlayer){
-            if(!p.hasPermission("townyports.set.spawn")){
-                p.sendMessage("You do not have permission to set the town port.");
-                return;
-            }
-        }
-
-        // Ensure the port location is actually inside of the town.
-        if(!town.isInsideTown(location)) {
-            if(calledByPlayer) p.sendMessage("This location is not inside your town.");
-            // elogger
-            return;
-        }
-
-        // Location safety check
-        if(!com.earthpol.earthPolLib.location.LocationUtil.isSafeLocation(location)) {
-            if(calledByPlayer) p.sendMessage("This location is not safe!");
-            return;
-        }
-
-        // Remove any existing port on this town.
-        removePort(town);
-
-        // Set the port metadata
-        MetaDataUtil.setLocation(town,portLocationField,location,true);
-        MetaDataUtil.setDouble(town,portPriceField,price,true);
-
+    public static boolean hasPort(Town town){
+        return getPort(town) != null;
     }
 
-    // Convenience override -- for calling with no player involved. (admin commands, etc.)
-    public static void setPort(Town town, Location portLocation, double portPrice){
-        setPort(town,portLocation,portPrice,null);
+    public static void setPortSpawn(Town town, Location location){
+        MetaDataUtil.setLocation(town,portLocationField,location,true);
+    }
+
+    public static void setPortPrice(Town town,double price){
+        MetaDataUtil.setDouble(town,portPriceField,price,true);
     }
 
     // Removes port metadata fields.
